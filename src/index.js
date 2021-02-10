1db6ba172567fdcc2abf37a46ebc97ead48c26c9
heyprashant <prashant.p7804@gmail.com>
2021-02-10 19:29:43 +0530

    add JWT

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000
 
+// app.use( (req, res, next) => {
+//     res.status(503).send('Site under maintenance, services are shudtownd temprarily')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,8 +19,13 @@ app.listen(port, () => {
 })
 
 const jwt = require('jsonwebtoken')
+
 const myFunction = async () => {
-    
+    const token = jwt.sign({_id: 'ab123'}, 'thisisnodecourse', {expiresIn: '1 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisisnodecourse')
+    console.log(data)
 }
 
 myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { findById } = require('../models/user')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
 router.post('/users', async (req, res) => {
@@ -8,23 +8,18 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
 router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
-
-    } catch (e) {
-        res.status(400).send()
-    }
+    res.send(req.user)
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
