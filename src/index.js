2486422366d557d1d347d6a8662bbb2ceafaa8c5
heyprashant <prashant.p7804@gmail.com>
2021-02-16 20:39:57 +0530

    add profle image upload feature

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000
 
-// app.use( (req, res, next) => {
-//     res.status(503).send('Site under maintenance, services are shudtownd temprarily')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -18,19 +14,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    const user = await User.findById('60263e097f2d27618003ae3a')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-// const main = async () => {
-//     const task = await Task.findById('60263ffc415085329478756f')
-//     await task.populate('owner').execPopulate()
-//     console.log(task.owner)
-// }
-
-// main()
