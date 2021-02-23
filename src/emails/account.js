13acf2b632650adb19888d51c786ac20d4c6d794
heyprashant <prashant.p7804@gmail.com>
2021-02-23 12:47:47 +0530

    Update dev config

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'prashant.p7804@gmail.com',
+        from: process.env.SGMAIL_EMAIL,
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'prashant.p7804@gmail.com',
+        from: process.env.SGMAIL_EMAIL,
         subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
