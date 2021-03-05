e5832ab1619bc93d5d88533680bbb8303142ed4c
heyprashant <prashant.p7804@gmail.com>
2021-03-05 21:06:33 +0530

    add Client

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const Task = require('../models/task')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
