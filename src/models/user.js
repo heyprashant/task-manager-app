cc3d14551e34fe3369126817d51fdbcb2b1cd3a9
heyprashant <prashant.p7804@gmail.com>
2021-03-22 22:03:34 +0530

    fix update avatar

--- src/models/user.js
+++ src/models/user.js
@@ -67,7 +67,7 @@ userSchema.methods.toJSON = function () {
 
     delete userObject.password
     delete userObject.tokens
-    delete userObject.avatar
+    // delete userObject.avatar
 
     return userObject
 }
