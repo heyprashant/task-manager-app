b5ee120bda0aa45b338133ad51319fc33458fcce
heyprashant <prashant.p7804@gmail.com>
2021-03-25 23:25:31 +0530

    update integrate client react app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 require('./db/mongoose')
+const path = require('path')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname,'../client/build')
+app.use(express.static(publicDirectoryPath))
+
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+
+app.get('*', (req, res) => {
+    console.log('route')
+    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+})
+
 
 module.exports = app
