55da2401b2257567f681b127d2dda73b1beea2ae
heyprashant <prashant.p7804@gmail.com>
2021-03-25 20:44:52 +0530

    Update email grid files

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,4 +1,5 @@
 const sgMail = require('@sendgrid/mail')
+const img = '../../assests/checked.png'
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -8,8 +9,11 @@ const sendWelcomeEmail = (email, name) => {
         from: process.env.SGMAIL_EMAIL,
         subject: 'Thanks for joining in!',
         html: `
-        <p>Welcome to the Task Manager app, ${name}.</p> 
-        <p>Let me know how you get along with the app.</p>`,
+        <p>Welcome to the Task Manager app, ${name}.
+        Let me know how you get along with the app.</p>
+        <br>
+        <h3>Task Manager </h3>
+        Prashant`
     })
 }
 
@@ -18,7 +22,12 @@ const sendCancelationEmail = (email, name) => {
         to: email,
         from: process.env.SGMAIL_EMAIL,
         subject: 'Sorry to see you go!',
-        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+        html: `
+            <p>Goodbye, ${name}.
+            I hope to see you back sometime soon.</p>
+            <br>
+            <h3>Task Manager </h3>
+            Prashant`
     })
 }
 
@@ -30,7 +39,10 @@ const sendPasswordResetEMail = (email, name, token) => {
         html: `
             <p>Hi ${name}</p>
             <p>Sorry to hear that you're having trouble with logging in to Task Manager. We can help you get straight back into your account.</p>
-            <p><a href='${process.env.CLIENT_URL}/user/password/reset/${token}'>Click here to reset password</a></p>`
+            <p><a href='${process.env.CLIENT_URL}/user/password/reset/${token}'>Click here to reset password</a></p>
+            <br>
+            <h3>Task Manager </h3>
+            Prashant`
             
     })
 }
